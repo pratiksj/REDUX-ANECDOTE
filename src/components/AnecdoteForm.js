0ec837ded16a9403596a0d0d8b35f32f806ef9d5
pratiksj <pratiksharai728@gmail.com>
2022-12-02 12:48:24 +0545

    exercise 6.7: separeated creation of new anecdotes form its own components

---
+++ src/components/AnecdoteForm.js
@@ -0,0 +1,27 @@
+import { useDispatch } from "react-redux";
+import { asObject } from "../reducers/anecdoteReducer";
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch();
+
+  const newAnecdote = (event) => {
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    event.target.anecdote.value = "";
+    dispatch(asObject(content));
+  };
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={newAnecdote}>
+        <div>
+          <input name="anecdote" />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  );
+};
+
+export default AnecdoteForm;
