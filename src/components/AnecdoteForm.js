4928ab71ea2bfd2f1bad75e2508dca0596663a83
pratiksj <pratiksharai728@gmail.com>
2022-12-21 23:49:32 +0545

    feat:6:20 modified the anecdoteForm component to use connect function  instead of hooks

--- src/components/AnecdoteForm.js
+++ src/components/AnecdoteForm.js
@@ -1,29 +1,14 @@
-import { useDispatch } from "react-redux";
+import { connect } from "react-redux";
 import { createAnecdote } from "../reducers/anecdoteReducer";
 import { setNotification } from "../reducers/notificationReducer";
-//import anecdoteService from "../services/anecdote";
 
-const AnecdoteForm = () => {
-  const dispatch = useDispatch();
-
-  // const newAnecdote = async (event) => {
-  //   event.preventDefault();
-  //   const content = event.target.anecdote.value;
-  //   event.target.anecdote.value = "";
-  //   const newAnecdote = await anecdoteService.createNew(content);
-  //   dispatch(createAnecdote(newAnecdote));
-  //   dispatch(setMessage(`${content} has added`));
-  //   setTimeout(() => {
-  //     dispatch(removeMessage());
-  //   }, 3000);
-  // };
-
+const AnecdoteForm = (props) => {
   const newAnecdote = (event) => {
     event.preventDefault();
     const content = event.target.anecdote.value;
     event.target.anecdote.value = "";
-    dispatch(createAnecdote(content));
-    dispatch(setNotification(`${content} has added`, 3));
+    props.createAnecdote(content);
+    props.setNotification(`${content} has added`, 3);
   };
 
   return (
@@ -40,4 +25,19 @@ const AnecdoteForm = () => {
   );
 };
 
-export default AnecdoteForm;
+const mapStateToProps = () => {
+  return {};
+};
+
+const mapDispatchToProps = {
+  createAnecdote,
+  setNotification,
+};
+
+// const ConnectedAnecdoteForm = connect(
+//   mapDispatchToProps,
+//   mapStateToProps///this step did not work
+// )(AnecdoteForm);
+
+// export default ConnectedAnecdoteForm;
+export default connect(mapStateToProps, mapDispatchToProps)(AnecdoteForm);
