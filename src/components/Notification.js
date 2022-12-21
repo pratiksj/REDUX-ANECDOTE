3cd5e2ab696b6da18e84a77767f40fb704a12b46
pratiksj <pratiksharai728@gmail.com>
2022-12-21 23:09:12 +0545

    feat:6:19 modified Notification componenet that uses connect function instead of useSelector api hook

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,17 +1,24 @@
-import { useSelector } from "react-redux";
+import { connect } from "react-redux";
+//import { useSelector } from "react-redux";
 
-const Notification = () => {
-  const notification = useSelector((state) => state.notification);
+const Notification = (props) => {
+  //const notification = useSelector((state) => state.notification);
   // console.log("hellow", notification);
   const style = {
     border: "solid",
     padding: 10,
     borderWidth: 1,
   };
-  if (notification === null) {
+  if (props.notification === null) {
     return null;
   }
-  return <div style={style}>{notification}</div>;
+  return <div style={style}>{props.notification}</div>;
+};
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification,
+  };
 };
 
-export default Notification;
+const ConnectedNotes = connect(mapStateToProps)(Notification);
+export default ConnectedNotes;
